f5b890e473414ce2c38de697d584bda60327ae92
Bill Mill <bill.mill@gmail.com>
2015-01-21 14:05:36 -0500

    test invalid dir

--- test/test.py
+++ test/test.py
@@ -1,6 +1,10 @@
 import slask
 
-def test_run():
+#TODO:
+# * configurable logging.
+# * test log messages.
+
+def test_plugin_success():
     hooks = slask.init_plugins("test/plugins")
     assert len(hooks) == 1
     assert "message" in hooks
@@ -8,3 +12,7 @@ def test_run():
     assert isinstance(hooks["message"], list)
     assert len(hooks["message"]) == 1
     assert hooks["message"][0]({"text": "bananas"}, None) == "bananas"
+
+def test_plugin_invalid_dir():
+    hooks = slask.init_plugins("invalid/package")
+    assert len(hooks) == 0
