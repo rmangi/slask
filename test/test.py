ac678e97c83c1c4a1be7e025c07deccd95ccb442
Bill Mill <bill.mill@gmail.com>
2015-01-21 13:58:24 -0500

    add some assertions

--- test/test.py
+++ test/test.py
@@ -2,4 +2,9 @@ import slask
 
 def test_run():
     hooks = slask.init_plugins("test/plugins")
-    print hooks
+    assert len(hooks) == 1
+    assert "message" in hooks
+    assert isinstance(hooks, dict)
+    assert isinstance(hooks["message"], list)
+    assert len(hooks["message"]) == 1
+    assert hooks["message"][0]({"text": "bananas"}, None) == "bananas"
